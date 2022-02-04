4b862b6ce51855ca20ad4ce3e9b2afed9e29f5d7
Rafael Lunardelli <rafael@devsquad.com>
2022-02-03 17:10:10 -0700

    deleting a todo

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
+
+    public function assignedTo()
+    {
+        return $this->belongsTo(User::class, 'assigned_to_id');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\Todo;
 use App\Mail\Invitation;
 use App\Models\Invites;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,11 +12,14 @@ Route::get('/', function () {
 
 Route::post('register', RegisterController::class)->name('register');
 
-Route::post('invite', function () {
-    Mail::to(request()->email)->send(new Invitation());
-    Invites::create(['email' => request()->email]);
-});
+Route::middleware('auth')->group(function () {
+    Route::post('invite', function () {
+        Mail::to(request()->email)->send(new Invitation());
+        Invites::create(['email' => request()->email]);
+    });
 
-Route::get('todo', Todo\IndexController::class)->name('todo.index');
-Route::post('todo', Todo\CreateController::class)->name('todo.store');
-Route::put('todo/{todo}', Todo\UpdateController::class)->name('todo.update');
+    Route::get('todo', Todo\IndexController::class)->name('todo.index');
+    Route::post('todo', Todo\CreateController::class)->name('todo.store');
+    Route::put('todo/{todo}', Todo\UpdateController::class)->name('todo.update');
+    Route::delete('todo/{todo}', Todo\DeleteController::class)->name('todo.destroy');
+});
